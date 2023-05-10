8f55daf829c1ae17155674e150f4d4d2e2c309fd
ArtemPurtoian <artemios.od@gmail.com>
2023-05-10 19:48:32 +0300

    vip - tuple, filter()

--- Purtoian_task_2_5_vip.py
+++ Purtoian_task_2_5_vip.py
@@ -1,7 +1,5 @@
-vip_room_guests = ['Anna', 'Bob', 'Chris', 'David']
-common_room_guests = {'Suzan', 'Martin', None, None}
+vip_room_guests = ('Anna', 'Bob', 'Chris', 'David')
+common_room_guests = ['Suzan', 'Martin', None, None]
 
-common_room_guests.discard(None)
-
-print(tuple(vip_room_guests))
-print(common_room_guests)
+print(vip_room_guests)
+print(list(filter(None, common_room_guests)))
