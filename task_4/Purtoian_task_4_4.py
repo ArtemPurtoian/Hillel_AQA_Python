d99769702e1160422bd84d802b6be1b39cb1cc68
ArtemPurtoian <artemios.od@gmail.com>
2023-05-19 10:26:10 +0300

    added task_4_4

---
+++ task_4/Purtoian_task_4_4.py
@@ -0,0 +1,36 @@
+import re
+
+
+my_html = '''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My super star task</title>
+</head>
+<body>
+    <div id="block-with-links">
+        <div id="google">
+            <a href="www.google.com">
+                Google
+            </a>
+        <div/>
+        <div id="facebook">
+            <a href="http://facebook.com/dign-in">
+                Facebook
+            </a>
+        </div>
+            <div id="amazon">
+                <a href="amazon.com">
+                    Amazon
+                </a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+'''
+
+pattern = r'<div id="(\w+)">\s*<a href="(.*?)">\s*(.*?)\s'
+result = re.findall(pattern, my_html)
+print(result)
