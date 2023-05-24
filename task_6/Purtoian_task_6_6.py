caacc57c0c4457141e9abacc70411fa743b10482
ArtemPurtoian <artemios.od@gmail.com>
2023-05-24 11:45:34 +0300

    added task_6_6

---
+++ task_6/Purtoian_task_6_6.py
@@ -0,0 +1,6 @@
+def calculate_sum(*numbers: int):
+    """
+    Create a function that accepts N parameters of type int.
+    Calculate the sum of these parameters and return it.
+    """
+    return sum(numbers)
