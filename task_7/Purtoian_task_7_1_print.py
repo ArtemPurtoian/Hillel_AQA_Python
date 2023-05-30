afeb106bbc202d256d5a9a1b72356219041223c4
ArtemPurtoian <artemios.od@gmail.com>
2023-05-30 22:17:36 +0300

    separated platforms, changed shell=False

--- task_7/Purtoian_task_7_1_print.py
+++ task_7/Purtoian_task_7_1_print.py
@@ -1,4 +1,5 @@
 import subprocess
+import sys
 
 
 def my_print(string_to_print: str):
@@ -6,4 +7,7 @@ def my_print(string_to_print: str):
     Implement your own print function. It should work on all operating systems.
     You can't use build-in print function
     """
-    subprocess.run(['echo', string_to_print], shell=True)
+    if sys.platform.startswith('win'):
+        subprocess.run(['echo', string_to_print], shell=True)
+    else:
+        subprocess.run(['echo', string_to_print], shell=False)
